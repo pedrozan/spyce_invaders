2aaf5d72ec63f6370483a15b707b61b819b38715
Pedro Henrique <pedro.schleder@gmail.com>
2019-05-30 20:35:17 -0300

    fixing typos

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,11 +12,11 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
-    def run_game(self)?
+    def run_game(self):
         """Start the main loop for the game."""
         while True:
             # Watch for keyboard and mouse events.
-            for event in pygame.event.get()?
+            for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
