3c416bb570889c5e2f7c3f5db6b966a0bc5e3675
Pedro Henrique <pedro.schleder@gmail.com>
2019-05-30 21:29:57 -0300

    refactoring keypresses event handlers

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,15 +35,27 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                if event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
                 if event.key == pygame.K_LEFT:
                     self.ship.moving_left = False
+    
+    def _check_keydown_events(self, event):
+        """Respond to key presses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        if event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+
+    def _check_keyup_events(self, event):
+        """Respond to key presses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        if event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
