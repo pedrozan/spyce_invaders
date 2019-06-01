2d550d881cd5f0766a0bd09b82a9837a3a1be5b5
Pedro Henrique Salton Schleder <pedro.schleder@cwi.com.br>
2019-05-31 21:02:50 -0300

    speeding up the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -66,6 +66,9 @@ class AlienInvasion:
         """Start a new game when the player clicks Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            # Reset the game settings.
+            self.settings.initialize_dynamic_settings()
+            
             # Hide the mouse cursor.
             pygame.mouse.set_visible(False)
 
@@ -170,6 +173,7 @@ class AlienInvasion:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _update_aliens(self):
         """
